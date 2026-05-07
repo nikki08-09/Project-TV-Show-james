26d19d21a52a23b0213456d178a8de0ff3a97a48
jamesishimwe <james123ishimwe@gmail.com>
2026-05-07 16:01:10 +0200

    Added a small div inside div with a name,title,season number , episode number and summary for each episode

--- script.js
+++ script.js
@@ -5,8 +5,27 @@ function setup() {
 }
 
 function makePageForEpisodes(episodeList) {
+  
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach((episode) => {
+    console.log(typeof(episode.season));
+    if(episode.season < 10) {
+        episode.season = " 0" + episode.season;
+        }
+      if(episode.number < 10) {
+        episode.number = " 0" + episode.number;
+          }
+    const episodeElem = document.createElement("div");
+    
+    episodeElem.classList.add("episode");
+    episodeElem.innerHTML = `
+      <h2>${episode.name}</h2>
+      <img src="${episode.image.medium}" alt="${episode.name}">
+     
+      <h3>Season ${episode.season}, Episode${episode.number}</h3>
+      <p>${episode.summary}</p>
+    `;
+    rootElem.appendChild(episodeElem);
+  });
 }
-
 window.onload = setup;
